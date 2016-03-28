8353472d7b3a07708d95f79789cf8b9d5cc8d254
ISHII 2bit <ishii@buffer-renaiss.com>
2016-03-28 16:20:36 +0900

    update example

--- ofxThreadPoolExample/src/ofApp.cpp
+++ ofxThreadPoolExample/src/ofApp.cpp
@@ -1,57 +1,70 @@
 #include "ofMain.h"
 #include "ofxThreadPool.h"
 
+#include <vector>
+
 class ofApp : public ofBaseApp {
-    ofxThreadPool<long, long> my_thread;
+    ofxThreadPool<std::string, ofJson> my_thread;
     bool bPush;
     std::size_t current;
+    std::string large_json_str;
 public:
     void setup() {
         bPush = false;
         current = 0;
         
-        my_thread.setup([](long l) -> double {
-            long u = l;
-            for(int i = 0; i < 30; i++) {
-                l = u;
-                while(l && l != 1) {
-                    if(l % 2 == 0) {
-                        l /= 2;
-                    } else {
-                        l = l * 3 + 1;
-                    }
-                }
-            }
-            return u;
-        }, [this](long l) {});
+        my_thread.setup([](std::string &&json_str) -> ofJson {
+            return ofJson::parse(json_str);
+        }, [this](ofJson &&l) {});
         
         my_thread.start();
         ofBackground(0, 0, 0);
         ofSetColor(255, 255, 255);
+        
+        ofJson json;
+        json["i"] = ofJson::array();
+        json["j"] = ofJson::array();
+        for(std::size_t i = 0; i < 20000; i++) {
+            json["i"][i] = i;
+            json["j"][i] = ofRandom(10.0);
+        }
+        
+        large_json_str = json.dump();
     }
     void update() {
-        if(bPush && ofGetFrameNum() % 3 == 0) {
-            for(int i = 0; i < 10000; i++) {
-                my_thread.push(current * 10000 + i);
+        if(ofGetFrameNum() % 3 == 0) {
+            if(bPush) {
+                for(int i = 0; i < 10; i++) {
+                    my_thread.push(large_json_str);
+                }
+                current++;
+            } else {
+                for(int i = 0; i < 10; i++) {
+                    ofJson json = ofJson::parse(large_json_str);
+                }
             }
-            current++;
         }
+        ofSetWindowTitle(ofToString(ofGetFrameRate()));
     }
     void draw() {
+        ofSetColor(bPush ? 0 : 255, bPush ? 255 : 0, 0);
+        ofDrawLine(ofGetFrameNum() % (int)ofGetWidth(), 0, ofGetFrameNum() % (int)ofGetWidth(), ofGetHeight());
+        ofSetColor(255, 255, 255);
+        ofDrawBitmapString(bPush ? "now parse in background" : "now parse in main thread", 20, 20);
         ofDrawBitmapString(ofVAArgsToString("queued:               %8d",
-                                            my_thread.queuedNum()), 20, 20);
+                                            my_thread.queuedNum()), 20, 50);
         ofDrawBitmapString(ofVAArgsToString("processed/frame:      %8d",
-                                            my_thread.processedPerFrame()), 20, 40);
+                                            my_thread.processedPerFrame()), 20, 70);
         ofDrawBitmapString(ofVAArgsToString("processed/thread:     %8.3f",
-                                            my_thread.processedPerThread()), 20, 60);
+                                            my_thread.processedPerThread()), 20, 90);
         ofDrawBitmapString(ofVAArgsToString("thread num:           %8d",
-                                            my_thread.threadNum()), 20, 80);
-        ofDrawBitmapString("space:   start/stop thread", 20, 110);
-        ofDrawBitmapString(std::string("         thread ") + (my_thread.isRunning() ? "running" : "stopped"), 20, 130);
-        ofDrawBitmapString("ENTER:   start/stop push queue", 20, 150);
-        ofDrawBitmapString(bPush ? "         pushing queue now" : "         doesn't push queue now", 20, 170);
-        ofDrawBitmapString("+:       add thread pool", 20, 190);
-        ofDrawBitmapString("-:       remove thread pool", 20, 210);
+                                            my_thread.threadNum()), 20, 110);
+        ofDrawBitmapString("space:   start/stop thread", 20, 140);
+        ofDrawBitmapString(std::string("         thread ") + (my_thread.isRunning() ? "running" : "stopped"), 20, 160);
+        ofDrawBitmapString("ENTER:   start/stop push queue", 20, 180);
+        ofDrawBitmapString(bPush ? "         pushing queue now" : "         doesn't push queue now", 20, 200);
+        ofDrawBitmapString("+:       add thread pool", 20, 220);
+        ofDrawBitmapString("-:       remove thread pool", 20, 240);
     }
     
     void exit() { my_thread.stop(); }
